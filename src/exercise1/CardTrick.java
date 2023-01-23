3623ead6035ec64ed9dc61b5ebff923e499338d9
SpellProgrammer <alesarah@sheridancollage.net>
2023-01-23 15:27:40 -0500

    ICE 1 Version 4

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -1,5 +1,6 @@
 package exercise1;
-
+import java.util.Random;
+import java.util.Scanner;
 /**
  * A class that fills a hand of 7 cards with random Card Objects and then asks the user to pick a card.
  * It then searches the array of cards for the match to the user's card. 
@@ -7,24 +8,29 @@ package exercise1;
  *
  * @author dancye
  * @author Paul Bonenfant Jan 25, 2022 
- * modifier sarah alexander
+ * modifier Sarah Alexander
  */
 public class CardTrick {
     
     public static void main(String[] args) {
-        
+        Random rand = new Random();
+        Scanner input = new Scanner(System.in);
         Card[] hand = new Card[7];
+        Card userCard = new Card();
 
         for (int i = 0; i < hand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            // 
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
-            // Hint: You can use Random -> random.nextInt(n) to get a random number between 0 and n-1 (inclusive)
-            //       Don't worry about duplicates at this point
+            card.setValue((int)(1+Math.random()*13));
+            card.setSuit(Card.SUITS[rand.nextInt(3)]);
         }
 
-        // insert code to ask the user for Card value and suit, create their card
+        System.out.println("What is the value of your card?");
+        int valueInput = input.nextInt();
+        userCard.setValue(valueInput);
+        
+        System.out.println("What is the suit of your card?");
+        String suitInput = input.next();
+        userCard.setSuit(suitInput);
         // and search the hand here. 
         // Hint: You can ask for values 1 to 10, and then
         //       11 for jack, 12 for queen, etc. (remember arrays are 0-based though)
